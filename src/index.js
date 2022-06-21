704fdbe23619b2f6e5a075b845c79458a3bd227d
shtag <vasyakanishchev@gmail.com>
2022-06-21 16:08:09 +0300

    feat:task complete

--- src/index.js
+++ src/index.js
@@ -38,8 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    console.log(expr);
+    let arr = [];
+    let result = [];
+    for(let i = 0; i < expr.length; i += 10){
+      let substr = expr.substr(i, 10);
+      arr.push(substr);
+    }
+    for(let i = 0; i < arr.length; i++){
+      let word;
+      word = arr[i].split('00').join('').split('10').join('.').split('11').join('-').split('**********').join('space');
+      result.push(word);
+    }
+    arr.length = 0;
+    result.forEach(element => {
+      let word2;
+      if(element === 'space') {word2 = ' ';
+    } else {word2 = MORSE_TABLE[element];}
+      arr.push(word2);  
+    })
+      return arr.join('');
+  }
 
 module.exports = {
     decode
